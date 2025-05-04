3f6bd0a3428c21fac6252b72b44d7300473d8999
ProbablyStupid <thomas.kaliwe@gmail.com>
2025-05-04 23:18:27 +0200

    Created basic popup window - no logic

---
+++ popup/index.js
@@ -0,0 +1,8 @@
+// TODO: implement button logic!
+
+// Step 1: load settings from browser storage
+
+// Step 2: update HTML elements to reflect browser storage
+
+// Step 3: interactivity -> enable or disable eradication of feeds
+// Step 3.1: save new settings to browser storage
